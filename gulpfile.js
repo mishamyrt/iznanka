ccce9b3559ea269c159c88c5ab3ca734e3634338
Misha <misha@myrt.co>
2017-06-09 13:13:24 +0300

    remove test from build

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,12 @@
-const cmd = require('node-cmd');
-const gulp = require('gulp');
-const zip = require('gulp-zip');
-const pkg = require('./package.json');
-
+const cmd     = require('node-cmd');
+const gulp    = require('gulp');
+const zip     = require('gulp-zip');
+const pkg     = require('./package.json');
 const release = './release';
-const build = './build';
-const source = './source';
+const build   = './build';
+const source  = './source';
 
-gulp.task('default', function() {
+gulp.task('default', function () {
     cmd.get('rm -r build');
     cmd.get('cp -r ' + source + ' ' + build);
     cmd.get('php minify.php ' + build + '/system/core.php');
@@ -16,7 +15,7 @@ gulp.task('default', function() {
     cmd.get('rm ' + build + '/system/tpl/test.tpl');
 });
 
-gulp.task('zip', function() {
+gulp.task('zip', function () {
     var version = pkg.version;
     return gulp.src(build + '/**/*', { dot: true })
         .pipe(zip('mishamyrt-iznanka-' + version + '.zip'))
